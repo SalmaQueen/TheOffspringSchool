2a2875f69dada239a3fb8536ef424875b20d2bfb
salmaQueen <salmawasee9@gmail.com>
2023-02-11 14:36:41 +0300

    made some changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
         onClick={()=>setToggle(toggle=>!toggle)}
         />
 
-        <div className={`${toggle ? 'flex' : 'hidden' } p-6 bg-blueDark  absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar `}>
+        <div className={`${toggle ? 'flex' : 'hidden' } p-6 bg-blueDark  absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-40 rounded-xl sidebar `}>
 
          <ul className='list-none flex  flex-col justify-end items-center flex-1'>
         {navLinks.map((nav, index)=>(
